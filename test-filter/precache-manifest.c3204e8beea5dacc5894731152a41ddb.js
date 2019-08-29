07ffb9dd84bc8c46bf38caf8cb7308cce77b7272
Danil <oldblacktree@gmail.com>
2019-08-29 14:32:22 +0300

    feat: build

---
+++ test-filter/precache-manifest.c3204e8beea5dacc5894731152a41ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c78c6e478978c84d320c4afd0e8359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92d4e81b4b67f83eb7e6",
+    "url": "/static/css/main.760aefcc.chunk.css"
+  },
+  {
+    "revision": "4a2d54799ce2415ab0db",
+    "url": "/static/js/2.fed68b4c.chunk.js"
+  },
+  {
+    "revision": "92d4e81b4b67f83eb7e6",
+    "url": "/static/js/main.dcde5b83.chunk.js"
+  },
+  {
+    "revision": "50823073bd64eff9db13",
+    "url": "/static/js/runtime~main.f27ca71d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
